c79d4d467458a5e31aea94816293b0c90ef9cfa1
harsh71019 <harsh710patel@gmail.com>
2022-03-23 00:24:45 +0530

    Adding redux saga setup and styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Aboutus from './pages/Aboutus';
+import AddEditUser from './pages/AddEditUser';
+import UserInfo from './pages/UserInfo';
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<Aboutus />} />
+        <Route path='/addUser' element={<AddEditUser />} />
+        <Route path='/editUser/:id' element={<AddEditUser />} />
+        <Route path='/userInfo/:id' element={<UserInfo />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Aboutus.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Aboutus = () => {
+  return <>Aboutus</>;
+};
+
+export default Aboutus;
